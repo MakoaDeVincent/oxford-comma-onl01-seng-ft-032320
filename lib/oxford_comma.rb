4565fef86c43131f450980f3fcdb9f702bb21523
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-25 21:49:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,5 @@
 def oxford_comma(array)
 
-
  if array.length <= 2
 
    array.join(", ")
@@ -8,7 +7,7 @@ def oxford_comma(array)
    array.join(" and ")
 
  else 
-   array[0...-1].join(", " + ", and " + array[-1])
+   array[0...-1].join(", ") + ", and " + array[-1])
 
  end
 end
