2f91dd06a0fecde8b333dc9bb01dd50395c6c340
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-25 21:39:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,13 +2,13 @@ def oxford_comma(array)
 
 
  if array.index <= 2
-   
+
    array.join(", ")
-   
+
    array.join(" and ")
 
  else
 
    array[0...-1].join(", " + " and " + array[-1])
-   
+
 end
